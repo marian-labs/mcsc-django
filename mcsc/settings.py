967e41154be14e4d110b05c9e4d6d37e4221af87
muhd-sinan-m <lmsinan772@gmail.com>
2026-08-02 14:11:19 +0530

    fix: switch to CompressedStaticFilesStorage and add build.sh to resolve WhiteNoise 500 error on Render

--- mcsc/settings.py
+++ mcsc/settings.py
@@ -155,7 +155,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files & Storage configuration
 USE_SUPABASE_STORAGE = config('USE_SUPABASE_STORAGE', default=False, cast=bool)
